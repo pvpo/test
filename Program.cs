7f1224901bd6dc846fe69394fab66181f4c051d4
pvpo <pavel.pavlov04@gmail.com>
2018-09-29 20:11:11 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using ConsoleApplication;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+
+namespace Visual_Studio
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var host = new WebHostBuilder()
+                    .UseKestrel()
+                    .UseStartup<Startup>()
+                    .UseIISIntegration()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .UseIISIntegration()
+                    .UseContentRoot(Directory.GetCurrentDirectory())
+                    .Build();
+            host.Run();        
+        }
+    }
+}
